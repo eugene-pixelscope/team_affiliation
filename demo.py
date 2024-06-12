8fbb7ee5eb6452882917e178130b2337c4d6c2e0
Eugene <hmkwon@pxscope.com>
2024-06-12 17:58:08 +0900

    add demo code

--- demo.py
+++ demo.py
@@ -1,37 +1,38 @@
-import os
-import cv2
-from modules import transform, resnet
-from modules.models import PRTreIDTeamClassifier
-import torch
-import numpy as np
-import os
 from glob import glob
-from kmeans_pytorch import kmeans
-from torchvision.transforms import Normalize
+import os
 from utils.vis_utils import show_mask_on_image
-np.random.seed(123)
 
-def preprocess(img, mean=None, std=None):
+from modules import resnet
+from modules.models import PRTreIDTeamClassifier
+from utils.model_utils import load_model
+
+import torch
+import cv2
+import numpy as np
+from fast_pytorch_kmeans import KMeans
+
+
+def preprocess(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (128,256))
+    img = cv2.resize(img, (128, 256))
     img = np.expand_dims(np.transpose(img, (2, 0, 1)), axis=0)
     result = torch.tensor(img, dtype=torch.float) / 255
-    if mean is not None and std is not None:
-        result = Normalize(mean=mean, std=std)(result)
     return result
 
+seed = 42
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+torch.cuda.manual_seed(seed)
+np.random.seed(seed)
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-backbone = resnet.get_resnet('ResNet34')
-# model_path = 'save/IceHockey/prtrei_resnet18_checkpoint_80.tar'
-model_path = 'save/SoccerNetGS/prtrei_resnet18_checkpoint_80.tar'
-attention_enable = True
-model = PRTreIDTeamClassifier(backbone=backbone, num_teams=12, attention_enable=attention_enable)
+backbone = resnet.get_resnet('ResNet18')
+model_path = 'weights/best.tar'
+model = PRTreIDTeamClassifier(backbone=backbone, num_teams=12, attention_enable=True)
 model = model.to(device)
 
-
 checkpoint = torch.load(model_path)
-model.load_state_dict(checkpoint['net'], strict=False)
+model = load_model(model, checkpoint)
 model.eval()
 
 # setting input
@@ -43,35 +44,29 @@ def set_input(dir_path):
 
     for k, path in enumerate(pathes):
         img = cv2.imread(path)
-        labels.append(['l', 'r'].index(os.path.basename(path).split('_')[0]))
+        labels.append(['1', '2'].index(os.path.basename(path).replace('.png','').split('-')[-1]))
         img = preprocess(img).to(device).float()
-        # img = preprocess(img, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]).to(device).float()
         inputs.append(img)
     return inputs, labels
 
-inputs, labels = set_input('demo_image/frame_1')
-print(labels)
+inputs, labels = set_input('demo_image')
 
 feature_vector = []
-for i, vector in enumerate(inputs):
+for i, input_tensor in enumerate(inputs):
     with torch.no_grad():
-        if attention_enable:
-            f, _, mask = model(vector)
-            B, C, _, _ = vector.shape
-            # visualization
-            for b in range(B):
-                vis_img = show_mask_on_image(img=vector[b].permute(1, 2, 0).cpu().detach().numpy(), mask=mask[b].cpu().detach().numpy(), image_weight=.7)
-                cv2.imwrite(f'output/{i}_{b}_map.jpg', vis_img)
-        else:
-            f = model.forward(vector)
-    f = f.detach().cpu().numpy()
-    feature_vector.extend(f)
+        f, _, mask = model(input_tensor)
+        # visualization
+        vis_img = show_mask_on_image(img=input_tensor.squeeze(0).permute(1, 2, 0).cpu().detach().numpy(),
+                                     mask=mask.squeeze(0).cpu().detach().numpy(),
+                                     image_weight=.7)
+        cv2.imwrite(f'output/{i}_map.jpg', vis_img)
+    feature_vector.append(f.squeeze(0))
+feature_vector = torch.stack(feature_vector)
+# kmeans
+kmeans = KMeans(n_clusters=2, mode='euclidean', verbose=0, max_iter=20)
+c = kmeans.fit_predict(feature_vector)
 
-feature_vector = torch.from_numpy(np.array(feature_vector))
-cluster_ids_x, cluster_centers = kmeans(
-    X=feature_vector, num_clusters=2, distance='cosine', device=device
-)
-
-print('dnn+kmeans: ', cluster_ids_x)
-
+pred = c.detach().cpu().numpy().tolist()
+print('pred: ', pred)
+print('gt: ', labels)
 print('Demo End')
