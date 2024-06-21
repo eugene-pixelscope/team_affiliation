e2fcd598d6e8cce90206f866bffb93c0c465c2ec
Eugene <hmkwon@pxscope.com>
2024-06-21 17:41:41 +0900

    modify demo code

--- demo.py
+++ demo.py
@@ -1,16 +1,21 @@
 from glob import glob
 import os
-
-import torch
-import cv2
-import numpy as np
+from utils.vis_utils import show_mask_on_image
 
 from modules import resnet, shufflenetv2, efficientnet
 from modules.models import PRTreIDTeamClassifier
 from utils.model_utils import load_model
-from utils.vis_utils import show_mask_on_image
-from sklearn.cluster import KMeans
+import torch.nn.functional as F
 
+import torch
+import cv2
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn_extra.cluster import CLARA
+import matplotlib.pyplot as plt
+from torchmetrics.functional.pairwise import pairwise_cosine_similarity
+import seaborn as sns
+from sklearn import preprocessing
 
 def preprocess(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -19,17 +24,23 @@ def preprocess(img):
     result = torch.tensor(img, dtype=torch.float) / 255
     return result
 
+# hyper param
+role_thr = 0.6
 
+# **********************
+# 0. Initialize setting
+# **********************
 seed = 42
 torch.manual_seed(seed)
 torch.cuda.manual_seed_all(seed)
 torch.cuda.manual_seed(seed)
 np.random.seed(seed)
-
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-model_path = 'weights/resnet18_aug:bright05:_NMI:05074_ARI:06174_F:08099_ACC:08931_withoutrole.tar'
-# model_path = 'save/Hockey/prtrei_resnet18_checkpoint_80_with_role.tar'
 
+# **********************
+# 1. Model construct
+# **********************
+model_path = 'save/Hockey/prtrei_resnet18_checkpoint_60_with_role.tar'
 if 'resnet' in os.path.basename(model_path):
     backbone = resnet.get_resnet('ResNet18')
 elif 'shufflenetv2' in os.path.basename(model_path):
@@ -42,8 +53,8 @@ else:
 model = PRTreIDTeamClassifier(backbone=backbone, num_teams=12, attention_enable=True)
 model = model.to(device)
 
-checkpoint = torch.load(model_path)
-model = load_model(model, checkpoint, filter_team_classifier=True)
+checkpoint = torch.load(model_path, map_location=device)
+load_model(model, checkpoint, filter_team_classifier=True)
 model.eval()
 
 # setting input
@@ -60,14 +71,19 @@ def set_input(dir_path):
         if c not in std_labels:
             continue
         labels.append(std_labels.index(c))
+        # **********************
+        # 2. Preprocessing
+        # **********************
         img = preprocess(img).to(device).float()
         inputs.append(img)
     return inputs, labels
 
-
 inputs, labels = set_input('demo_image')
 pred_labels = []
-feature_tensors = []
+feature_vector = []
+# **********************
+# 3. Inference
+# **********************
 for i, input_tensor in enumerate(inputs):
     with torch.no_grad():
         f, _, mask, role_cls = model(input_tensor)
@@ -77,26 +93,35 @@ for i, input_tensor in enumerate(inputs):
                                  mask=mask.squeeze(0).cpu().detach().numpy(),
                                  image_weight=.7)
     cv2.imwrite(f'output/{i}_map.jpg', vis_img)
+    feature_vector.append(f.squeeze(0))
+    pred_labels.append(0)
 
-    if not bool(role) or bool(role_cls.squeeze(0)[role] < 0.7):
-        feature_tensors.append(f.squeeze(0))
-        pred_labels.append(0)
-    else:
-        pred_labels.append(2)
+# **********************
+# 4. Postprocessing
+# **********************
+feature_vector = torch.stack(feature_vector)
+feature = feature_vector.detach().cpu().numpy()
+nor_feature = preprocessing.normalize(feature)
 
-feature_tensors = torch.stack(feature_tensors)
-feature_vectors = feature_tensors.detach().cpu().numpy()
-# kmeans
-kmeans = KMeans(n_clusters=2, random_state=seed, max_iter=20, n_init=10)
-kmeans.fit(feature_vectors)
-pred = kmeans.labels_
+# **********************
+# 5. Clustering
+# **********************
+# clustering_engine = KMeans(n_clusters=2, random_state=seed, max_iter=20, n_init=10)
+clustering_engine = CLARA(n_clusters=2, random_state=seed, max_iter=20)
+clustering_engine.fit(nor_feature)
+pred = clustering_engine.labels_
+
+out = pairwise_cosine_similarity(feature_vector)
+out.fill_diagonal_(1)
+plt.clf()
+ax = sns.heatmap(out.detach().cpu().numpy(), linewidth=0.5, annot=True, fmt=".2f")
+plt.savefig(f'output/cosine.png')
 
 c_idx = 0
 for i in range(len(pred_labels)):
     if pred_labels[i] == 0:
         pred_labels[i] += pred[c_idx]
         c_idx += 1
-
 print('pred: ', pred_labels)
 print('gt: ', labels)
 print('Demo End')
--- demo_role_cls.py
+++ demo_role_cls.py
@@ -10,8 +10,12 @@ import torch.nn.functional as F
 import torch
 import cv2
 import numpy as np
-from fast_pytorch_kmeans import KMeans
-
+from sklearn.cluster import KMeans
+from sklearn_extra.cluster import CLARA
+import matplotlib.pyplot as plt
+from torchmetrics.functional.pairwise import pairwise_cosine_similarity
+import seaborn as sns
+from sklearn import preprocessing
 
 def preprocess(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -20,14 +24,23 @@ def preprocess(img):
     result = torch.tensor(img, dtype=torch.float) / 255
     return result
 
+# hyper param
+role_thr = 0.6
+
+# **********************
+# 0. Initialize setting
+# **********************
 seed = 42
 torch.manual_seed(seed)
 torch.cuda.manual_seed_all(seed)
 torch.cuda.manual_seed(seed)
 np.random.seed(seed)
-
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-model_path = 'weights/best_with_role.tar'
+
+# **********************
+# 1. Model construct
+# **********************
+model_path = 'save/Hockey/prtrei_resnet18_checkpoint_60_with_role.tar'
 if 'resnet' in os.path.basename(model_path):
     backbone = resnet.get_resnet('ResNet18')
 elif 'shufflenetv2' in os.path.basename(model_path):
@@ -41,12 +54,12 @@ model = PRTreIDTeamClassifier(backbone=backbone, num_teams=12, attention_enable=
 model = model.to(device)
 
 checkpoint = torch.load(model_path)
-model = load_model(model, checkpoint, filter_team_classifier=True)
+load_model(model, checkpoint, filter_team_classifier=True)
 model.eval()
 
 # setting input
 def set_input(dir_path):
-    std_labels = ['1', '2', '3']
+    std_labels = ['1', '2', '3', '4']
     inputs = []
     labels = []
     pathes = glob(os.path.join(dir_path, '*.png'))
@@ -58,6 +71,9 @@ def set_input(dir_path):
         if c not in std_labels:
             continue
         labels.append(std_labels.index(c))
+        # **********************
+        # 2. Preprocessing
+        # **********************
         img = preprocess(img).to(device).float()
         inputs.append(img)
     return inputs, labels
@@ -65,28 +81,47 @@ def set_input(dir_path):
 inputs, labels = set_input('demo_image')
 pred_labels = []
 feature_vector = []
+# **********************
+# 3. Inference
+# **********************
 for i, input_tensor in enumerate(inputs):
     with torch.no_grad():
         f, _, mask, role_cls = model(input_tensor)
-    # visualization
+
     role = torch.argmax(role_cls, dim=1)
+    # visualization
     vis_img = show_mask_on_image(img=input_tensor.squeeze(0).permute(1, 2, 0).cpu().detach().numpy(),
                                  mask=mask.squeeze(0).cpu().detach().numpy(),
                                  image_weight=.7)
     cv2.imwrite(f'output/{i}_map.jpg', vis_img)
-
-    if not bool(role) or bool(role_cls.squeeze(0)[role] < 0.7):
+    print(role_cls, role)
+    if bool(role):
+        pred_labels.append(2)
+    else:
         feature_vector.append(f.squeeze(0))
         pred_labels.append(0)
-    else:
-        pred_labels.append(2)
 
+# **********************
+# 4. Postprocessing
+# **********************
 feature_vector = torch.stack(feature_vector)
-# kmeans
-kmeans = KMeans(n_clusters=2, mode='euclidean', max_iter=100)
-c = kmeans.fit_predict(feature_vector)
+feature = feature_vector.detach().cpu().numpy()
+nor_feature = preprocessing.normalize(feature)
 
-pred = c.detach().cpu().numpy().tolist()
+# **********************
+# 5. Clustering
+# **********************
+# clustering_engine = KMeans(n_clusters=2, random_state=seed, max_iter=20, n_init=10)
+clustering_engine = CLARA(n_clusters=2, random_state=seed, max_iter=20)
+clustering_engine.fit(nor_feature)
+pred = clustering_engine.labels_
+
+out = pairwise_cosine_similarity(feature_vector)
+out.fill_diagonal_(1)
+plt.clf()
+ax = sns.heatmap(out.detach().cpu().numpy(), linewidth=0.5, annot=True, fmt=".2f")
+plt.savefig(f'output/cosine.png')
+
 c_idx = 0
 for i in range(len(pred_labels)):
     if pred_labels[i] == 0:
